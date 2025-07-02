87388e994403286cfe924d21f7c050a586792730
IXID7OMIXI <aboda7m4ever@gmail.com>
2025-07-03 01:24:36 +0300

    added a new font type, bg pic, maybe a minigame to add also not sure

--- src/App.tsx
+++ src/App.tsx
@@ -1,75 +1,18 @@
-import React, { useState } from "react";
+import React from 'react';
 
-const App: React.FC = () => {
-    // Explicitly type the state as number
-    const [counter, setCounter] = useState<number>(0);
-
-    // Optionally, type the event handlers
-    const handleClick1 = (): void => {
-        setCounter(counter + 1);
-    };
-
-    const handleClick2 = (): void => {
-        setCounter(counter - 1);
-    };
-
-    return (
-        <div
-            style={{
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "center",
-                justifyContent: "center",
-                fontSize: "300%",
-                position: "absolute",
-                width: "100%",
-                height: "100%",
-                top: "-15%",
-                backgroundColor: "#e0f7fa"
-            }}
-        >
-            <h1>Counter App</h1>
-            <div
-                style={{
-                    fontSize: "120%",
-                    position: "relative",
-                    top: "10vh",
-                }}
-            >
-                {counter}
-            </div>
-            <div className="buttons">
-                <button
-                    style={{
-                        fontSize: "60%",
-                        position: "relative",
-                        top: "20vh",
-                        marginRight: "5px",
-                        backgroundColor: "green",
-                        borderRadius: "8%",
-                        color: "white",
-                    }}
-                    onClick={handleClick1}
-                >
-                    Increment
-                </button>
-                <button
-                    style={{
-                        fontSize: "60%",
-                        position: "relative",
-                        top: "20vh",
-                        marginLeft: "5px",
-                        backgroundColor: "red",
-                        borderRadius: "8%",
-                        color: "white",
-                    }}
-                    onClick={handleClick2}
-                >
-                    Decrement
-                </button>
-            </div>
-        </div>
-    );
-};
+function App() {
+  return (
+    
+    <h1
+      style={{
+        fontFamily: "'Press Start 2P",
+        fontSize: '8rem',
+        textAlign:'center',
+        marginTop: '600px'
+        }}>  
+        Counter App
+    </h1>
+  );
+}
 
 export default App;
