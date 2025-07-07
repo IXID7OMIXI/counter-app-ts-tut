79de66ed91f71a8a0de5dcad3d6823d76c8b3489
IXID7OMIXI <aboda7m4ever@gmail.com>
2025-07-07 21:19:16 +0300

    mobile version fix

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import bg from './pic/bg-gif.gif';
 import Counter from './components/counter';
 import Running from './components/running';
+
 function Top() {
   return (
     <h1 className='pixel-letters'>  
@@ -10,16 +11,17 @@ function Top() {
   );
 }
 function Main()
+
 {
   return (
     <div
       className='bg-main'
       style={{ backgroundImage: `url(${bg})`}}>
-      <div>
+      
       <Top />
       <Counter />
       <Zooming />
-      </div>
+      
     </div>
   );
 }
--- src/components/counter.tsx
+++ src/components/counter.tsx
@@ -8,12 +8,19 @@ function Counter() {
   return (
     <div style={{ textAlign: 'center' }}>
       <h1 className='pixel-letters'>{y}</h1>
-      <Grid container  
+        <Grid container  
         spacing={15}
-        justifyContent="center"
-      >
-      <button className="pixel-button" onClick={() => x(y + 1)}>Found it! </button>
-      <button className="pixel-button"  onClick={() => x(y - 1)}>i lied :( </button>
+        justifyContent="center">
+
+            <button className="pixel-button"
+                onClick={() => x(y + 1)}
+                onTouchStart={() => x(y + 1)}>
+                Found it!</button>
+
+            <button className="pixel-button"
+                onClick={() => x(y - 1)}
+                onTouchStart={() => x(y - 1)}>
+                i lied :(</button>
       </Grid>
     </div>
   );
